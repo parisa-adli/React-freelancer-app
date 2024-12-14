60b687272c0ea25786ad98686d7296b6b05bcc87
parisa-adli <parisa.adli1996@gmail.com>
2024-12-14 17:35:19 +0330

    send otp handler

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import Textfield from "../../ui/Textfield";
+import { useMutation } from "@tanstack/react-query";
+import { getOtp } from "../../services/authService";
+import toast from "react-hot-toast";
 
-function SentOTPForm() {
+function SendOTPForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
 
-  const sendOtpHandler = (e) => {
+  const { isPending, error, data, mutateAsync } = useMutation({
+    mutationFn: getOtp,
+  });
+
+  const sendOtpHandler = async (e) => {
     e.preventDefault();
+    try {
+      const data = await mutateAsync({ phoneNumber });
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
   };
 
   return (
@@ -25,4 +38,4 @@ function SentOTPForm() {
   );
 }
 
-export default SentOTPForm;
+export default SendOTPForm;
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,12 +1,12 @@
-import SentOTPForm from "../features/authentication/SentOTPForm";
-import CheckOTPForm from "../features/authentication/CheckOTPForm";
+import SendOTPForm from "../features/authentication/SendOTPForm";
+// import CheckOTPForm from "../features/authentication/CheckOTPForm";
 
 function Auth() {
   return (
     <div className="flex justify-center pt-10">
       <div className="w-full sm:max-w-sm">
-        <SentOTPForm />
-        <CheckOTPForm />
+        <SendOTPForm />
+        {/* <CheckOTPForm /> */}
       </div>
     </div>
   );
