b91d61d5ae1535c29af4339c07520f6e9a0f9d01
parisa-adli <parisa.adli1996@gmail.com>
2024-12-15 10:59:49 +0330

    complete profile

--- src/features/authentication/CompleteProfileForm.jsx
+++ src/features/authentication/CompleteProfileForm.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 import Textfield from "../../ui/Textfield";
 import RadioInput from "../../ui/RadioInput";
+import { useMutation } from "@tanstack/react-query";
+import { completeProfile } from "../../services/authService";
+import Loading from "../../ui/Loading";
+import toast from "react-hot-toast";
 
 function CompleteProfileForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("");
 
+  const { isPending, mutateAsync } = useMutation({
+    mutationFn: completeProfile,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { user, message } = await mutateAsync({ name, email, role });
+      console.log(user, message);
+      toast.success(message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
   return (
     <div className="flex justify-center pt-10">
       <div className="w-full sm:max-w-sm">
-        <form className="space-y-8">
+        <form className="space-y-8" onSubmit={handleSubmit}>
           <Textfield
             label="نام و نام خانوادگی"
             name="name"
@@ -41,7 +60,11 @@ function CompleteProfileForm() {
               checked={role === "FREELANCER"}
             />
           </div>
-          <button className="btn btn--primary w-full">تایید</button>
+          {isPending ? (
+            <Loading />
+          ) : (
+            <button className="btn btn--primary w-full">تایید</button>
+          )}
         </form>
       </div>
     </div>
