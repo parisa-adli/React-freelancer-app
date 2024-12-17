20b93f56afd4efcb516ca3cc48fc29fa65444050
parisa-adli <parisa.adli1996@gmail.com>
2024-12-17 16:08:14 +0330

    useOutsideClick

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,18 +1,8 @@
-import { useEffect, useRef } from "react";
 import { HiOutlineX } from "react-icons/hi";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 function Modal({ open, onClose, title, children }) {
-  const ref = useRef();
-
-  useEffect(() => {
-    function handleClick(e) {
-      if (ref.current && !ref.current.contains(e.target)) {
-        onClose();
-      }
-    }
-    document.addEventListener("click", handleClick, true);
-    return () => document.removeEventListener("click", handleClick, true);
-  }, [onClose]);
+  const ref = useOutsideClick(onClose);
 
   return (
     open && (
