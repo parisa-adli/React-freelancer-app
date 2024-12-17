791a9b1d4aeb43ddffe10b767e48e76b7f8caee1
parisa-adli <parisa.adli1996@gmail.com>
2024-12-17 16:01:16 +0330

    click outside modal-close modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,14 +1,27 @@
+import { useEffect, useRef } from "react";
 import { HiOutlineX } from "react-icons/hi";
 
 function Modal({ open, onClose, title, children }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    function handleClick(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        onClose();
+      }
+    }
+    document.addEventListener("click", handleClick, true);
+    return () => document.removeEventListener("click", handleClick, true);
+  }, [onClose]);
+
   return (
     open && (
       <div
-        onClick={onClose}
         className="backdrop-blur-sm fixed inset-0
            w-full h-screen bg-secondary-800 bg-opacity-30 z-50"
       >
         <div
+          ref={ref}
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
         rounded-lg bg-secondary-0 p-4 shadow-lg transition-all duration-500 ease-out
         w-[calc(100vw-2rem)] sm:max-w-md max-h-[calc(100vh-2rem)] overflow-y-auto"
