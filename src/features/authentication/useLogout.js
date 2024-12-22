bfe6ce5198282a527f7d497f28561a3e1840921d
parisa-adli <parisa.adli1996@gmail.com>
2024-12-22 10:10:40 +0330

    user logout

---
+++ src/features/authentication/useLogout.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { logoutApi } from "../../services/authService";
+import { useNavigate } from "react-router-dom";
+
+export default function useLogout() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { isPending, mutate: logout } = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: (data) => {
+      queryClient.removeQueries();
+      navigate("/auth", { replace: true });
+    },
+
+    onError: (err) => toast.error(err?.response?.data?.message),
+  });
+  return { isPending, logout };
+}
---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,10 @@
+import { HiOutlineSun } from "react-icons/hi"
+
+function DarkModeToggle() {
+  return (
+    <div>
+      <HiOutlineSun className="h-5 w-5 text-primary-900" />
+    </div>
+  );
+}
+export default DarkModeToggle
