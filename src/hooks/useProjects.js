298c7a21eb2607429476e30fc587920a3fbce269
parisa-adli <parisa.adli1996@gmail.com>
2024-12-26 10:59:15 +0330

    convert query string to query object

--- src/hooks/useProjects.js
+++ src/hooks/useProjects.js
@@ -1,10 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { getProjectsApi } from "../services/projectService";
+import { useLocation } from "react-router-dom";
 
 export default function useProjects() {
+  const { search } = useLocation();
+  console.log(search);
+
+  // const queryObject = queryString.parse(search);
+  const queryObject = Object.fromEntries(new URLSearchParams(search));
+  console.log(queryObject);
+
   const { data, isLoading } = useQuery({
-    queryKey: ["projects"],
-    queryFn: getProjectsApi,
+    queryKey: ["projects", queryObject],
+    queryFn: () => getProjectsApi(search),
   });
 
   const { projects } = data || {};
