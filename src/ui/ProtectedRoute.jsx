a003a64b11a19430ad3b0692509632955ee51de8
parisa-adli <parisa.adli1996@gmail.com>
2024-12-26 12:04:43 +0330

    protected route permission

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,9 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import useAuthorize from "../features/authentication/useAuthorize";
+import { useEffect } from "react";
+import Loading from "../ui/Loading";
 
 function ProtectedRoute({ children }) {
-  const { isLoading, isAuthenticated, isAuthorized } = useAuthorize();
-  console.log(isAuthorized);
+  const negative = useNavigate();
 
-  return children;
+  // 1. load the authenticated user
+  const { isLoading, isAuthenticated, isAuthorized } = useAuthorize();
+
+  // 2. check if is Authorized or not, check is Authenticated or not
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) negative("/auth");
+    if (!isAuthorized && !isLoading) negative("/not-access", { replace: true });
+  }, [isLoading, isAuthenticated, isAuthorized, negative]);
+
+  // 3. while loading => show loading
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-screen bg-secondary-100">
+        <Loading />
+      </div>
+    );
+
+  // 4. if user isAuthenticated and isAuthorized => render the app
+  if (isAuthenticated && isAuthorized) return children;
 }
 export default ProtectedRoute;
