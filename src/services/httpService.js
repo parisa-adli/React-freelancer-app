611a40f98c29f28594981b8f57af3892c6269f49
parisa <parisa.adli1996@gmail.com>
2025-12-29 17:06:11 +0330

    Change BASE_URL to production endpoint

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://freelancer-bc.runflare.run/";
 
 const app = axios.create({
   baseURL: BASE_URL,
