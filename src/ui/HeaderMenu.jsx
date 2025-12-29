796348147f5f041988c04dfb9f9a8147d46967cd
parisa-adli <parisa.adli1996@gmail.com>
2025-12-29 18:03:18 +0330

    fix login issue

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,17 +1,28 @@
 import DarkModeToggle from "./DarkModeToggle";
-import Logout from "../features/authentication/Logout";
 import UserAvatar from "../features/authentication/UserAvatar";
+import { useNavigate } from "react-router-dom";
+import { HiArrowLeftOnRectangle } from "react-icons/hi2";
+import useUser from "../features/authentication/useUser";
 
 function HeaderMenu() {
+  const { user } = useUser();
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/auth", { replace: true });
+  };
   return (
     <ul className="flex items-center gap-x-4">
       <UserAvatar />
       <li className="flex">
         <DarkModeToggle />
       </li>
-      <li className="flex hover:text-rose-500">
-        <Logout />
-      </li>
+      {!user && (
+        <li className="flex hover:text-rose-500">
+          <button onClick={handleClick}>
+            <HiArrowLeftOnRectangle className="h-5 w-5 text-secondary-500 hover:text-success" />
+          </button>
+        </li>
+      )}
     </ul>
   );
 }
